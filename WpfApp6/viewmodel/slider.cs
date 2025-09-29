c58cedab3b8393075182f2a06e0cb150a6bd81a6
Ashutosh Chudasama <ashutoshchudasama989@gmail.com>
2025-09-29 13:30:35 +0530

    Add files via upload

---
+++ WpfApp6/viewmodel/slider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6.viewmodel
+{
+    internal class slider :INotifyPropertyChanged
+    {
+		private int _slidervalue;
+
+		public int slidervalue
+		{
+			get { return _slidervalue; }
+			set { _slidervalue = value;
+				OnPropertyChanged(nameof(slidervalue));
+			}
+		}
+
+        private void OnPropertyChanged(string v)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v)); 
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}

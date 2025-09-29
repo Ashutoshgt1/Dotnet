e0ab96db2e93cecd15448dd9e66a94132f9d19b5
Ashutosh Chudasama <ashutoshchudasama989@gmail.com>
2025-09-29 13:34:11 +0530

    Add files via upload

---
+++ WpfApp5/viewmodel1/viewmodel1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5.viewmodel
+{
+    class viewmodel1:INotifyPropertyChanged
+	{
+		private string _name;
+
+		public string name
+		{
+			get { return _name; }
+			set { _name = value; 
+				Onpropertychanged(nameof(name));
+			}
+		}
+
+        private void Onpropertychanged(string v)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
